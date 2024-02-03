6fa90ca91a194476461c291c8c85851ef63e3359
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2024-02-03 23:00:20 +0100

    Prepare common components using another injector

--- service-consumer-ng/src/app/clients/graphql/index.ts
+++ service-consumer-ng/src/app/clients/graphql/index.ts
@@ -1,13 +1,11 @@
 import {EnvironmentProviders, Injector, Provider} from '@angular/core';
 import {API_ENDPOINT, provideEnvironment} from '@app/environment';
-import {provideHttpClient} from '@angular/common/http';
 import {BLOGPOST_SERVICE} from '@app/clients';
 import {GraphQlBlogPostService} from './blogpost.service';
 
 export function provideGraphQlClient(): Provider | EnvironmentProviders[] {
   return [
     provideEnvironment(),
-    provideHttpClient(),
     {
       provide: BLOGPOST_SERVICE,
       useFactory: (injector: Injector) => new GraphQlBlogPostService(
